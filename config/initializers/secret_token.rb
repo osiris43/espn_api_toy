e3a43cebcaaf1ddaf15211f8a912e61327d64e87
Brett Bim <osiris43@gmail.com>
2012-03-05 13:16:30 -0600

    initial espn toy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EspnApiToy::Application.config.secret_token = '9d68757e2a74055d68c93eac0839a2b8f928f13be18a4d1e6c87a42bdd70138a98ad37d5a037c57087c0f507e0cf9b9d4806458942c15b188f0cf8b8f4123ae6'
